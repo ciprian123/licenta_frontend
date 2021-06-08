0255ef3b8f69ba46eb1740a3ea99126d3c811478
Ciprian Ursulean <ciprian.ursulean5@gmail.com>
2021-06-08 22:39:52 +0300

    Addeed machine learning functionality

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -15,8 +15,7 @@
                     value="2018-07-22"
                     min="2018-01-01" max="2025-12-31">
                 <p id="prediction_result">Prediction result: </p>
-                <p id="predicted_quantity">Quantity: 126</p>
-                <p id="predicted_probability">Probability: 0.87</p>
+                <p id="predicted_quantity">Quantity: </p>
             </div>
         </div>
     </div>
@@ -94,7 +93,6 @@ export default ({
         // }).catch(err => console.log(err));
 
         const drugListData = await axios.get('http://localhost:8080/api/v1/drugs/distinct-names').catch(err => console.log(err));
-        console.log(drugListData);
         this.drugList = drugListData.data;
 
         const quantities = await axios.get('http://localhost:8080/api/v1/drugs/get-quantities/?name=' + 'acarbosum').catch(err => console.log(err));
@@ -110,61 +108,30 @@ export default ({
         forceRerender() {
             this.componentKey += 1;
         },
-        generateResults() {
+        async generateResults(event) {
             const predicted_quantity = document.querySelector("#predicted_quantity");
-            const predicted_probability = document.querySelector("#predicted_probability");
             predicted_quantity.innerHTML = "Quantity: " + Math.random() * 123;
-            predicted_probability.innerHTML = "Probability" + 0.2 + Math.random();
-            console.log("E bine!");
-        },
-        generateChartConfig(idx) {
-            this.forceRerender();
-            if (idx == 1) {
-                console.log("111111111111111");
-                this.type = "line",
-                this.label = "Quantity",
-                this.labels = ['12.02.2020', '16.02.2020', '1.03.2020', '2.03.2020', '9.03.2020', '12.03.2020', '12.03.2020', '16.03.2020', '13.03.2020', '2.04.2020', '9.04.2020', '12.04.2020'],
-                this.data = [100, 99, 70, 120, 86, 56, 61, 56, 12, 11, 17, 30],
-                this.backgroundColor = '#EF6C00',
-                this.background = '#EF6C00'
-            } else if (idx == 2) {
-                console.log("2222222222222222");
-                this.type = "line",
-                this.label = "Quantity",
-                this.labels = ['2.02.2020', '6.02.2020', '11.03.2020', '12.03.2020', '19.03.2020', '22.03.2020', '23.03.2020', '26.03.2020', '27.03.2020', '2.04.2020', '9.04.2020', '12.04.2020'],
-                this.data = [10, 19, 90, 120, 186, 156, 161, 256, 12, 81, 107, 130],
-                this.backgroundColor = '#69F0AE',
-                this.background = '#69F0AE'
-            } else if (idx == 3) {
-                this.type = "line",
-                this.label = "Quantity",
-                this.labels = ['2.02.2021', '6.02.2021', '11.03.2021', '12.03.2021', '19.03.2021', '22.03.2021', '23.03.2021', '26.03.2021', '27.03.2021', '2.04.2021', '9.04.2021', '12.04.2021'],
-                this.data = [210, 49, 240, 120, 386, 256, 161, 356, 102, 181, 207, 330],
-                this.backgroundColor = '#2962FF',
-                this.background = '#2962FF'
-            } else if (idx == 4) { 
-                this.type = "line",
-                this.label = "Quantity",
-                this.labels = ['2.02.2021', '6.02.2021', '11.03.2021', '12.03.2021', '19.03.2021', '22.03.2021', '23.03.2021', '26.03.2021', '27.03.2021', '2.04.2021', '9.04.2021', '12.04.2021'],
-                this.data = [10, 49, 40, 20, 86, 56, 61, 56, 12, 81, 27, 89],
-                this.backgroundColor = '#6200EA',
-                this.background = '#6200EA'
-            } else if (idx == 5) {
-                this.type = "line",
-                this.label = "Quantity",
-                this.labels = ['2.02.2021', '6.02.2021', '11.03.2021', '12.03.2021', '19.03.2021', '22.03.2021', '23.03.2021', '26.03.2021', '27.03.2021', '2.04.2021', '9.04.2021', '12.04.2021'],
-                this.data = [100, 490, 400, 200, 860, 560, 610, 560, 120, 810, 270, 890],
-                this.backgroundColor = '#000000',
-                this.background = '#000000'
+            console.log(event.target.value);
+
+            const selected_drug = document.querySelector("#drug_list").value;
+            console.log(selected_drug);
+
+            let date_to_predict = event.target.value;
+            let toTimestamp = Date.parse(date_to_predict) / 1000;
+            console.log(toTimestamp)
+            const predicted_result = await axios.get('http://127.0.0.1:5000/predict?drug=' + selected_drug + '&&timestamp=' + toTimestamp).catch(err => console.log(err));
+            if (Number.parseFloat(predicted_result.data) > 0) {
+                document.querySelector("#predicted_quantity").innerHTML = "Quantity: " + predicted_result.data;
+            } else {
+                document.querySelector("#predicted_quantity").innerHTML = "Warning: not enough data to predict";
             }
+            console.log(predicted_result.data)
         },
         async updateChart(event) {
             // this.generateChartConfig(event.target.value);
             const drugName = event.target.value;
             const quantities = await axios.get('http://localhost:8080/api/v1/drugs/get-quantities/?name=' + drugName).catch(err => console.log(err));
             const dates = await axios.get('http://localhost:8080/api/v1/drugs/get-dates/?name=' + drugName).catch(err => console.log(err));
-            console.log(quantities.data);
-            console.log(dates.data);
 
             this.labels = dates.data;
             for (let i = 0; i < this.labels.length; ++i) {
@@ -172,7 +139,6 @@ export default ({
             }
             this.data = quantities.data;
             this.forceRerender();
-            console.log("aici");
         }
     }
 })
